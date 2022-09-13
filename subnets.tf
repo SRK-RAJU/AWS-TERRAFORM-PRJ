1b5324a775951cffed17e01144f731a4f81894cc
91994 <suddala.rajukumar123@gmail.com>
2022-09-13 20:49:13 +0530

    updating

--- subnets.tf
+++ subnets.tf
@@ -1,6 +1,6 @@
 #Creating Public subnet
 
-resource "aws_subnet" "public-sub-1" {
+resource "aws_subnet" "public-sub" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "172.31.0.0/28"
   availability_zone = "us-east-1a"
@@ -9,25 +9,25 @@ resource "aws_subnet" "public-sub-1" {
   tags = merge(
     local.tags,
     {
-      Name = "my_vpc-pub-sub-1"
+      Name = "my_vpc-pub-sub"
     })
 }
-resource "aws_subnet" "public-sub-2" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "172.31.0.16/28"
-  availability_zone = "us-east-1a"
-  enable_resource_name_dns_a_record_on_launch="true"
-  map_public_ip_on_launch = "true"
-  tags = merge(
-    local.tags,
-    {
-      Name = "my_vpc-pvt-sub-2"
-    })
-}
+#resource "aws_subnet" "public-sub-2" {
+#  vpc_id     = aws_vpc.my_vpc.id
+#  cidr_block = "172.31.0.16/28"
+#  availability_zone = "us-east-1a"
+#  enable_resource_name_dns_a_record_on_launch="true"
+#  map_public_ip_on_launch = "true"
+#  tags = merge(
+#    local.tags,
+#    {
+#      Name = "my_vpc-pvt-sub-2"
+#    })
+#}
 
 #creating private subnet
 
-resource "aws_subnet" "private-sub-1" {
+resource "aws_subnet" "private-sub" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = "172.31.0.32/28"
   availability_zone = "us-east-1b"
@@ -35,20 +35,20 @@ resource "aws_subnet" "private-sub-1" {
   tags = merge(
     local.tags,
     {
-      Name = "my_vpc-pvt-sub-1"
+      Name = "my_vpc-pvt-sub"
     })
 }
-resource "aws_subnet" "private-sub-2" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "172.31.0.48/28"
-  availability_zone = "us-east-1b"
-  enable_resource_name_dns_a_record_on_launch="true"
-  tags = merge(
-    local.tags,
-    {
-      Name = "my_vpc-pvt-sub-2"
-    })
-}
+#resource "aws_subnet" "private-sub-2" {
+#  vpc_id     = aws_vpc.my_vpc.id
+#  cidr_block = "172.31.0.48/28"
+#  availability_zone = "us-east-1b"
+#  enable_resource_name_dns_a_record_on_launch="true"
+#  tags = merge(
+#    local.tags,
+#    {
+#      Name = "my_vpc-pvt-sub-2"
+#    })
+#}
 #creating route tables
 
 
@@ -63,10 +63,10 @@ resource "aws_internet_gateway" "my_vpc_igw" {
     })
 }
 
-resource "aws_internet_gateway_attachment" "igw-attach"{
-  internet_gateway_id = aws_internet_gateway.my_vpc_igw.id
-  vpc_id              = aws_vpc.my_vpc.id
-}
+#resource "aws_internet_gateway_attachment" "igw-attach"{
+#  internet_gateway_id = aws_internet_gateway.my_vpc_igw.id
+#  vpc_id              = aws_vpc.my_vpc.id
+#}
 
 #resource "aws_internet_gateway_attachment" "igw-attach" {
 #  internet_gateway_id=aws_internet_gateway.my_vpc_igw.id
@@ -74,7 +74,7 @@ resource "aws_internet_gateway_attachment" "igw-attach"{
 #}
 resource "aws_nat_gateway" "dev-nat" {
   allocation_id = aws_eip.nat-eip.id
-  subnet_id = aws_subnet.public-sub-1.id
+  subnet_id = aws_subnet.public-sub.id
   tags={
     Name="Nat Gateway"
   }
