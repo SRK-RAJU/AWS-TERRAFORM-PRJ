b35a3dc72d2678bfa8fd31d6b0f190a0637d1a4d
91994 <suddala.rajukumar123@gmail.com>
2022-09-15 20:27:04 +0530

    updating

--- main.tf
+++ main.tf
@@ -213,7 +213,7 @@ sudo yum update -y
 echo "Install Docker engine"
 sudo yum install -y docker
 sudo sudo chkconfig docker on
-sudo service docker start
+sudo service dock er start
 sudo usermod -a -G docker ec2-user
 sudo docker pull nginx:latest
 sudo docker run --name mynginx1 -p 70:80 -d nginx
