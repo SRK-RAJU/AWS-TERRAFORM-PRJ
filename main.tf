b86ebf97c69930f7c4773c316e6fd66e4957af71
91994 <suddala.rajukumar123@gmail.com>
2022-09-15 17:19:20 +0530

    updating

--- main.tf
+++ main.tf
@@ -372,45 +372,45 @@ resource "aws_launch_configuration" "launch_config" {
   security_groups             = [aws_security_group.default.id]
 
   associate_public_ip_address = true
-  user_data = <<-EOF
-#! /bin/bash
-sudo yum update -y
-sudo yum install -y docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-sudo docker pull nginx:latest
-sudo docker run --name mynginx2 -p 70:80 -d nginx
-
-sudo yum -y update
-
-echo "Install Java JDK 8"
-sudo yum remove -y java
-sudo yum install -y java-1.8.0-openjdk
-
-echo "Install Maven"
-sudo yum install -y maven
-
-echo "Install git"
-sudo yum install -y git
-
-echo "Install Docker engine"
-sudo yum update -y
-sudo yum install docker -y
-sudo sudo chkconfig docker on
-
-echo "Install Jenkins"
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-sudo yum install -y jenkins
-sudo usermod -a -G docker jenkins
-sudo chkconfig jenkins on
-
-echo "Start Docker & Jenkins services"
-sudo service docker start
-sudo service jenkins start
-sudo wget https://get.jenkins.io/war-stable/2.361.1/jenkins.war
-sudo java -jar jenkins.war
-EOF
+#  user_data = <<-EOF
+  ##! /bin/bash
+#sudo yum update -y
+#sudo yum install -y docker
+#sudo service docker start
+#sudo usermod -a -G docker ec2-user
+#sudo docker pull nginx:latest
+#sudo docker run --name mynginx2 -p 70:80 -d nginx
+#
+#sudo yum -y update
+#
+#echo "Install Java JDK 8"
+#sudo yum remove -y java
+#sudo yum install -y java-1.8.0-openjdk
+#
+#echo "Install Maven"
+#sudo yum install -y maven
+#
+#echo "Install git"
+#sudo yum install -y git
+#
+#echo "Install Docker engine"
+#sudo yum update -y
+#sudo yum install docker -y
+#sudo sudo chkconfig docker on
+#
+#echo "Install Jenkins"
+#sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+#sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+#sudo yum install -y jenkins
+#sudo usermod -a -G docker jenkins
+#sudo chkconfig jenkins on
+#
+#echo "Start Docker & Jenkins services"
+#sudo service docker start
+#sudo service jenkins start
+#sudo wget https://get.jenkins.io/war-stable/2.361.1/jenkins.war
+#sudo java -jar jenkins.war
+#EOF
 
   #       user_data = <<-EOF
 #                #!/bin/bash
