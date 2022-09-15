da06ff1ea55fc0ad6e2f4ab8946d1b8f6b192256
91994 <suddala.rajukumar123@gmail.com>
2022-09-15 14:50:55 +0530

    updating

--- main.tf
+++ main.tf
@@ -123,6 +123,64 @@ resource "aws_key_pair" "generated_key" {
 ##  }
 #}
 
+# Set the required provider and versions
+terraform {
+  required_providers {
+    # We recommend pinning to the specific version of the Docker Provider you're using
+    # since new versions are released frequently
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "2.21.0"
+    }
+  }
+}
+
+# Configure the docker provider
+provider "docker" {
+}
+
+# Create a docker image resource
+# -> docker pull nginx:latest
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+# Create a docker container resource
+# -> same as 'docker run --name nginx -p8080:80 -d nginx:latest'
+resource "docker_container" "nginx" {
+  name    = "nginx"
+  image   = docker_image.nginx.image_id
+
+  ports {
+    external = 8080
+    internal = 80
+  }
+}
+
+# Or create a service resource
+# -> same as 'docker service create -d -p 8081:80 --name nginx-service --replicas 2 nginx:latest'
+resource "docker_service" "nginx_service" {
+  name = "nginx-service"
+  task_spec {
+    container_spec {
+      image = docker_image.nginx.repo_digest
+    }
+  }
+
+  mode {
+    replicated {
+      replicas = 2
+    }
+  }
+
+  endpoint_spec {
+    ports {
+      published_port = 8081
+      target_port    = 80
+    }
+  }
+}
 
 resource "aws_instance" "app_server-pub" {
   ami           = "ami-05fa00d4c63e32376"
@@ -131,7 +189,7 @@ resource "aws_instance" "app_server-pub" {
   security_groups = [ aws_security_group.allow-sg-pub.id ]
   subnet_id = aws_subnet.public-sub.id
 #  associate_public_ip_address = true
-  user_data = "user.sh"
+  user_data = "user.tpl"
 #  user_data = "${file("user.tpl")}"
   #  count = 2
 
