8ed757c1d58d4a2740022568e1960aeb699abb89
91994 <suddala.rajukumar123@gmail.com>
2022-09-15 16:14:05 +0530

    updating

--- main.tf
+++ main.tf
@@ -182,16 +182,6 @@ resource "aws_key_pair" "generated_key" {
 #  }
 #}
 
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-    }
-  }
-}
-
-provider "docker" {}
 
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
--- providers.tf
+++ providers.tf
@@ -14,8 +14,23 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
   }
 }
+#terraform {
+#  required_providers {
+#    docker = {
+#      source  = "kreuzwerker/docker"
+#      version = "~> 2.13.0"
+#    }
+#  }
+#}
+
+provider "docker" {}
+
 
 terraform {
     backend "s3" {
